b6b20d9ae065942c13ad6440fb06ba745d01b26c
Said Aboudou Mamadou <saaboudo@1H3.42angouleme.fr>
2023-12-13 20:08:32 +0100

    Secondcommit

--- test_read.c
+++ test_read.c
@@ -1,17 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   test_read.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: saaboudo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/13 16:38:19 by saaboudo          #+#    #+#             */
-/*   Updated: 2023/12/13 20:07:49 by saaboudo         ###   ########.fr       */
+/*   Created: 2023/12/13 19:33:07 by saaboudo          #+#    #+#             */
+/*   Updated: 2023/12/13 20:07:44 by saaboudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h> 
 
-char	*get_next_line(int fd)
+
+/* size_t read(int fd, void *buf, size_t count) prototype of read() func */
+
+int main(void)
 {
-	
+	int fd; 
+	char buff[1024];
+	char path[] = "Documents/42cursus/get_next_line"
+
+
+
 }
