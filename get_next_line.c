df3aef466bed54cd4a104e15b7a96716fabaf075
Said Aboudou Mamadou <saaboudo@1G4.42angouleme.fr>
2023-12-19 18:46:32 +0100

    10thCommit

--- get_next_line.c
+++ get_next_line.c
@@ -5,13 +5,164 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: saaboudo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/13 16:38:19 by saaboudo          #+#    #+#             */
-/*   Updated: 2023/12/14 14:31:10 by saaboudo         ###   ########.fr       */
+/*   Created: 2023/12/17 15:03:59 by saaboudo          #+#    #+#             */
+/*   Updated: 2023/12/19 18:30:05 by saaboudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "get_next_line.h"
+
+static char *append_str(char *pool_buffer, char *read_buffer);
+
+static char *read_from_fd(char *pool_buffer, int fd)
+{
+	int byte_read; 
+	char *scoop_buffer; 
+	
+	scoop_buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	if (scoop_buffer == NULL)
+		return (NULL);
+	byte_read = 1;
+	while (byte_read > 0)
+	{
+		byte_read = read(fd, scoop_buffer,BUFFER_SIZE);
+		if (byte_read == -1)
+		{
+			free(scoop_buffer);
+			return (NULL);
+		}
+		scoop_buffer[byte_read] = '\0';
+		pool_buffer = append_str(pool_buffer, scoop_buffer);
+		if (ft_strchr(pool_buffer, '\n'))
+			break ;
+
+	}
+	free(scoop_buffer);
+	return (pool_buffer);
+}
+
+static char *append_str(char *pool_buffer,char *read_buffer)
+{
+	char *tmp; 
+
+	tmp = ft_strjoin(pool_buffer, read_buffer);
+	free(pool_buffer);
+	return (tmp);
+}
+
+char *extract_line (char *buffer)
+{
+	char *new_line_pos;
+	size_t line_length;
+	char *line;
+
+	if(buffer == NULL || *buffer == '\0')
+	{
+		return (NULL);
+	}
+
+	new_line_pos = ft_strchr(buffer, '\n');
+	if (new_line_pos == NULL)
+		return (NULL);
+	line_length = new_line_pos - buffer;
+	line =(char*) malloc((line_length + 2) * sizeof(char));
+
+	strncpy(line, buffer, line_length);
+	line[line_length] = '\0';
+
+	return (line);
+}
+
+
+char *obtainremaining(char *buffer)
+{
+	if (buffer == NULL || *buffer == '\0')
+	{
+		return (NULL);
+	}
+
+	char *new_pos = ft_strchr(buffer, '\n');
+
+	if (new_pos == NULL)
+	{
+		 return (NULL);
+	}	
+	size_t remaining_length = ft_strlen(new_pos + 1);
+
+	memmove(buffer, new_pos + 1, remaining_length + 1);
+
+	return (buffer);
+}
+
+
 
 char	*get_next_line(int fd)
 {
-		
+	static char *basin_buffer = NULL;
+	char *line;
+
+	line = NULL;
+	if(fd < 0 || read (fd,NULL,0) < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if(!basin_buffer)
+		basin_buffer = ft_calloc(1, sizeof (char));
+	if (!ft_strchr(basin_buffer, '\n'))
+		basin_buffer = read_from_fd(basin_buffer, fd);
+	if (!basin_buffer || basin_buffer[0] == '\0')
+	{
+		free(line);
+		free(basin_buffer);
+		return (NULL);
+	}
+	line = extract_line(basin_buffer);
+	basin_buffer = obtainremaining(basin_buffer);
+	return (line);
+}
+
+
+/*
+int main(void)
+{
+
+	
+	int fd; 
+
+	char *line;
+	 int count;
+
+	count = 0;
+	fd = open("example.txt", O_RDONLY);
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		count++;
+		printf("[%d]:%s\n", count, line);
+		free (line);
+	}
+	close(fd);
+	 return(0);
+}
+*/
+
+int    main(void)
+{
+    int    fd;
+    int    i;
+    char *str;
+
+    i = 0;
+    fd = open("example.txt", O_RDONLY);
+		while (i < 4)
+    {
+        str =  get_next_line(fd);
+        printf("%s", str);
+        //free(str);
+        i++;
+    }
+    close (fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: saaboudo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 14:05:48 by saaboudo          #+#    #+#             */
-/*   Updated: 2023/12/17 15:22:27 by saaboudo         ###   ########.fr       */
+/*   Updated: 2023/12/19 18:13:40 by saaboudo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@
 # include <stddef.h>
 #endif 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 3
+# define BUFFER_SIZE 1
 #endif
 
+char	*get_next_line(int fd);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strdup(const char *s);
 void	*ft_calloc(size_t nmemb, size_t size);
 void	*ft_memset(void *str, int c, size_t n);
 size_t	ft_strlen(const char *s);
+
